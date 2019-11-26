89e2cf92c0e3227df21333b554f5773e2377642c
Navdeep Singh Rathore <navdeepsinghrathore2000@gmail.com>
2019-11-26 09:35:09 +0530

    The python code for automation has been added

---
+++ sium.py
@@ -0,0 +1,40 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options 
+from bs4 import BeautifulSoup
+import time
+# we are doing thiss to avoid constant pop-ups that do not let us access the exact html that we want to 
+
+chromedriver = '/chromerdriver'
+chrome_options = webdriver.ChromeOptions()
+prefs = {"profile.default_content_setting_values.notifications" : 2}
+chrome_options.add_experimental_option("prefs",prefs)
+browser = webdriver.Chrome(chrome_options=chrome_options)
+browser.get('https://www.facebook.com')
+
+
+ls = []
+with open("file.txt",'r') as file :
+	for line in file :
+		ls.append(line.strip())
+# strip() method is used to remove the /n at the end of each line
+
+# we start by just taking one acc detail for now
+# You can later try to expand it for number of acccounts
+
+num_friends = 0
+
+if len(ls) == 2 :
+	#logging in
+	browser.find_element_by_id('email').send_keys(ls[0])
+	browser.find_element_by_id('pass').send_keys(ls[1])
+	
+	#navigation
+	browser.find_element_by_id('loginbutton').click()
+	browser.find_element_by_xpath('//*[@id="u_0_a"]/div[1]/div[1]/div/a').click()
+
+
+	time.sleep(3)
+	browser.find_element_by_xpath('//*[@id="u_fetchstream_2_a"]/li[3]/a').click()
+	
+
+
